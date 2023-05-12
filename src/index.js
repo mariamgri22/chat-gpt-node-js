a4737242c52ef020af14b9b1afd1eb2beb3c1fb2
mariamgri22 <mariamgrigoryan99@gmail.com>
2023-05-12 20:06:47 +0400

    fix:input.txt length validation

--- src/index.js
+++ src/index.js
@@ -5,26 +5,32 @@ import { Configuration, OpenAIApi } from "openai";
 const prompt = fs.readFileSync("./input.txt", "utf8");
 dotenv.config();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+if (prompt.trim().length === 0) {
+  
+  console.log("The input file is empty.");
+} 
+else {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
 
-async function chatGPTAnswer() {
-  try {
-    const completion = await openai.createChatCompletion ({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: `${prompt}` }],
-    });
-    console.log(completion.data.choices[0].message.content);
-  } catch (error) {
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-    } else {
-      console.log(error.message);
+  async function chatGPTAnswer() {
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: `${prompt}` }],
+      });
+      console.log(completion.data.choices[0].message.content);
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
     }
   }
-}
 
-chatGPTAnswer();
+  chatGPTAnswer();
+}
